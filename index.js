240590e43f9c49300c268e62e2e50a19d65dcc25
Amit Yizraeli <amit-yiz@noal.org.il>
2020-07-14 10:28:23 +0300

    without the comment

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ let btnNumbers = document.getElementsByClassName("btn-num");
 
 console.log(document.getElementsByClassName("btn").length); // result=
 
-/* for (let i = 0; i<btnNumbers.length; i++) {
+for (let i = 0; i<btnNumbers.length; i++) {
     btnNumbers[i].addEventListener("click", () => {
         console.log(this.dataset.calc);
     });
-} */
+}
 
 document.getElementById("light-dark").addEventListener("click", () => {
     console.log("toogle");
