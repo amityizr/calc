52f1cef1763420dcb551f06a47cd087aeea2d7d2
Amit Yizraeli <amit-yiz@noal.org.il>
2020-07-14 10:19:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let btnNumbers = document.getElementsByClassName("btn-num");
+
+console.log(document.getElementsByClassName("btn").length); // result=
+
+/* for (let i = 0; i<btnNumbers.length; i++) {
+    btnNumbers[i].addEventListener("click", () => {
+        console.log(this.dataset.calc);
+    });
+} */
+
+document.getElementById("light-dark").addEventListener("click", () => {
+    console.log("toogle");
+    document.body.classList.toggle("body-dark");
+});
+
+
+//let btnAction = document.getElementsByClassName("btn-action");
+//let btnEqual = document.getElementsByClassName("btn-equal");
+
+//btnAction.addEventListener("click", displayDate);
+//btnEqual.addEventListener("click", displayDate);
+
+//parseInt(this.dataset.calc);
